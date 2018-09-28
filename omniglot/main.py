4dd35c1b9c516b5cc94db2b5a94ab98f911c49dc
yueying-teng <yt2495@columbia.edu>
2018-09-28 16:05:03 +0900

    Update main.py

--- omniglot/main.py
+++ omniglot/main.py
@@ -2,6 +2,13 @@ import data_loader
 import modified_sgd 
 import siamese_network
 
+momentum = 0.9
+# linear epoch slope evolution
+momentum_slope = 0.01
+support_set_size = 20
+evaluate_each = 500
+number_of_train_iterations = 100000
+
 
 validation_accuracy = siamese_network.train_siamese_network(number_of_iterations = number_of_train_iterations, support_set_size = support_set_size,\
                                                             final_momentum = momentum, momentum_slope = momentum_slope, evaluate_each = evaluate_each,\
