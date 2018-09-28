938bb85f9cec62af2e42975aaac4218bb2492d5c
yueying-teng <yt2495@columbia.edu>
2018-09-28 15:59:32 +0900

    Update main.py

--- omniglot/main.py
+++ omniglot/main.py
@@ -3,8 +3,9 @@ import modified_sgd
 import siamese_network
 
 
-validation_accuracy = siamese_network.train_siamese_network(number_of_iterations = number_of_train_iterations, support_set_size = support_set_size, final_momentum = momentum, \
-                                            momentum_slope = momentum_slope, evaluate_each = evaluate_each, model_name = 'siamese_net_lr10e-4')
+validation_accuracy = siamese_network.train_siamese_network(number_of_iterations = number_of_train_iterations, support_set_size = support_set_size,\
+                                                            final_momentum = momentum, momentum_slope = momentum_slope, evaluate_each = evaluate_each,\
+                                                            model_name = 'siamese_net_lr10e-4')
 if validation_accuracy == 0:
     evaluation_accuracy = 0
 else:
